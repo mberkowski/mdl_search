add8c30fa5b05820651fa387a40c355afd33aa02
Chad Fennell <fenne035@umn.edu>
2016-03-28 14:16:18 -0500

    removes solr reboot (will be using monit for this); adds linked dirs for image caches

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -9,31 +9,8 @@ set :ssh_options, {:forward_agent => true}
 set :deploy_to, '/swadm/var/www/html/mdl'
 set :use_sudo, false
 
-# set :linked_dirs, fetch(:linked_dirs) + %w(public/uploads public/embeds log cache tmp solr_prod)
+set :linked_dirs, fetch(:linked_dirs) + %w(thumbnails cdm_images tmp log cache)
 
 set :rails_env, "production"
 
 set :bundle_flags, '--deployment'
-
-namespace :deploy do
-  desc "Restart Application"
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, "#{release_path}/tmp/restart.txt"
-    end
-  end
-
-  desc "Start Solr instance"
-  task :start_solr do
-    on roles(:app) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "solr:setup_prod"
-        end
-      end
-    end
-  end
-
-  after 'deploy:updated', 'deploy:start_solr'
-  after :publishing, :restart
-end
