ae9b12e6f559a68a6e8d1b1b6037bd692b93e070
Chad Fennell <fenne035@umn.edu>
2016-03-31 16:51:03 -0500

    bl expects these unused params; just make them optional

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -1,6 +1,6 @@
 module ThumbnailHelper
 
-  def cached_thumbnail_tag(document)
+  def cached_thumbnail_tag(document, image_options = nil)
     image_tag  thumbnail_path(*document['id'].split(':'))
   end
 
