163117e1b2fd81ddbde89b73e6a1c6601d788fc4
chadfennell <fenne035@umn.edu>
2016-09-14 15:43:36 -0500

    updateded schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303202611) do
+ActiveRecord::Schema.define(version: 20160914180623) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20160303202611) do
   end
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
+
+  create_table "indexing_runs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "searches", force: :cascade do |t|
     t.text     "query_params"
