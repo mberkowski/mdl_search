fb741a2cd5abcef15076479c525ff290b725f156
chadfennell <libsys@gmail.com>
2016-10-20 15:33:33 -0500

    use the new CDMBL namespace

--- config/initializers/cdmbl.rb
+++ config/initializers/cdmbl.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'uri'
-module CDM2Solr
+module CDMBL
   class CompletedCallback
     def self.call!
       Rails.logger.info "Swapping Solr Cores Following Indexing Status: #{Net::HTTP.get_response(URI(ENV['SOLR_URL']))}"
