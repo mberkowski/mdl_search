829b08db0afc3c3fe90bbdd696f8597ba250a10e
chadfennell <libsys@gmail.com>
2016-12-13 11:31:35 -0600

    take advantage of new cdmbl loader hook

--- config/initializers/cdmbl.rb
+++ config/initializers/cdmbl.rb
@@ -18,4 +18,11 @@ module CDMBL
       Rails.logger.info "CDMBL: Requesting: #{collection}:#{id} from #{endpoint}"
     end
   end
+
+  # An example callback
+  class LoaderNotification
+    def self.call!(ingestables, deletables)
+      Rails.logger.info "CDMBL: Loading #{ingestables.length} records and deleting #{deletables.length}"
+    end
+  end
 end
