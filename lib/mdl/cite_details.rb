b1ff8a4dacbdefdae95b736de252d873447baa40
chadfennell <libsys@gmail.com>
2017-02-21 12:19:25 -0600

    a few requested cite details changes

--- lib/mdl/cite_details.rb
+++ lib/mdl/cite_details.rb
@@ -41,28 +41,28 @@ module MDL
     end
 
     def field_values(values, key, facet)
-      values.map do |val| 
+      values.map do |val|
         [{text: val}, {url: facet_url(key, val, facet)}].inject({}) do |memo, item|
           (item.values.first) ? memo.merge(item) : memo
         end
       end
     end
 
-    def facet_url(key, val, facet = false) 
+    def facet_url(key, val, facet = false)
       (facet && val) ? URI.escape("/?f[#{key}][]=#{val}") : nil
     end
 
     def details_fields
       [
         {key: 'title_ssi', label: 'Title'},
-        {key: 'dat_ssi', label: 'Date'},
         {key: 'creator_ssim', label: 'Creator', delimiter: ', ', facet: true},
-        {key: 'contributing_organization_ssi', label: 'Contributing Organization', facet: true},
+        {key: 'dat_ssi', label: 'Date'},
         {key: 'description_ts', label: 'Description'},
-        {key: 'contributor_ssim', label: 'Contributor', delimiter: ', ', facet: true},
-        {key: 'publishing_agency_ssi', label: 'Publishing Agency', facet: true},
+        {key: 'contributing_organization_ssi', label: 'Contributing Organization', facet: true},
         {key: 'type_ssi', label: 'Type', facet: true},
         {key: 'physical_format_ssi', label: 'Format', facet: true},
+        {key: 'contributor_ssim', label: 'Contributor', delimiter: ', ', facet: true},
+        {key: 'publishing_agency_ssi', label: 'Publishing Agency', facet: true},
         {key: 'dimensions_ssi', label: 'Dimensions', facet: true},
         {key: 'country_ssi', label: 'Country', facet: true},
         {key: 'state_ssi', label: 'State', facet: true},
@@ -76,8 +76,6 @@ module MDL
         {key: 'contact_information_ssi', label: 'Contact information'},
         {key: 'formal_subject_ssim', label: 'Formal Subjects', delimiter: ', ', facet: true},
         {key: 'subject_ssim', label: 'Local Subjects', delimiter: ', ', facet: true},
-        {key: 'topic_ssim', label: 'Topic', delimiter: ', ', facet: true},
-        {key: 'keyword_ssim', label: 'Keywords', delimiter: ', ', facet: true},
         {key: 'fiscal_sponsor_ssi', label: 'Fiscal Sponsor'},
         {key: 'geonam_ssi', label: 'GeoNames Identifier'}
       ]
