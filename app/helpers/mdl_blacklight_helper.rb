e1b4a6c0bf0c4a01a674b9559a668d0e6cb56304
chadfennell <libsys@gmail.com>
2017-03-28 12:16:20 -0500

    include a link to json results

--- app/helpers/mdl_blacklight_helper.rb
+++ app/helpers/mdl_blacklight_helper.rb
@@ -1,6 +1,18 @@
 module MdlBlacklightHelper
   include Blacklight::LayoutHelperBehavior
 
+  def current_search
+    request.original_url
+  end
+
+  def current_search_json
+     current_search.gsub(/\/catalog/, '\catalog.json')
+  end
+
+  def json_result_link
+    link_to(raw('<div class="icon-json pull-right"></div>'), current_search_json)
+  end
+
   def record_count
     number_with_delimiter(Blacklight.default_index.connection.get('select',
       :params => { :q => '*:*',
