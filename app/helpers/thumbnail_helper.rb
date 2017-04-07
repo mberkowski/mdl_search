349fc5eebba2cd0b822edbb322efd90b274be0b8
Chad Fennell <fenne035@umn.edu>
2017-04-07 14:56:03 -0500

    quick demo spike for the team

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -2,4 +2,8 @@ module ThumbnailHelper
   def cached_thumbnail_tag(document, image_options = nil)
     image_tag thumbnail_path(document['id'], type: document['type_ssi'])
   end
+
+  def thumbnail_link(document)
+    link_to("#{document['title_ssi'].gsub(/'/, "\\'")} <br /> #{cached_thumbnail_tag(document)}".html_safe, "/catalog/#{document['id']}")
+  end
 end
