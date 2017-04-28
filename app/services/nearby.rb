63793bab2965ea804090ea7abb08495bf8efae09
Chad Fennell <fenne035@umn.edu>
2017-04-28 14:42:05 -0500

    push out the default distance a bit

--- app/services/nearby.rb
+++ app/services/nearby.rb
@@ -1,6 +1,6 @@
 # A wrapper around a solr bounding box query
 class Nearby
-  def self.search(q: '*:*', pt: '46.7296,-94.6859', d: 15)
+  def self.search(q: '*:*', pt: '46.7296,-94.6859', d: 35)
     Blacklight.default_index.connection.get('select',
       :params => { :q => "#{q} -coordinates_llsi:\"#{pt}\"",
         :d => d,
