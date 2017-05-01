572c9e7c11ecdac421a2bf3eeec09f234ba93210
Chad Fennell <fenne035@umn.edu>
2017-05-01 16:02:06 -0500

    cache the home page search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,6 +4,22 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
 
 
+  def run_search!
+    search_results(params)
+  end
+
+  # get search results from the solr index
+  def index
+    if params == {"controller"=>"catalog", "action"=>"index"}
+      @response, @document_list = Rails.cache.fetch("home_search", expires_in: 12.hours) do
+        run_search!
+      end
+    else
+      @response, @document_list = run_search!
+    end
+    super
+  end
+
 # updates the search counter (allows the show view to paginate)
   def track
     search_session['counter'] = params[:counter]
