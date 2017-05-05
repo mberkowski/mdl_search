5f0ff9f438f3ee96d5a834d357538b33f7404d72
Chad Fennell <fenne035@umn.edu>
2017-05-05 14:09:54 -0500

    avoid ruby warnings, don't use this reserved term

--- lib/mdl/initial_path.rb
+++ lib/mdl/initial_path.rb
@@ -1,13 +1,13 @@
 module MDL
   class InitialPath
-    attr_reader :format, :path_klass
-    def initialize(format: format, path_klass: MDL::BorealisAssetMap)
-      @format = format
+    attr_reader :format_field, :path_klass
+    def initialize(format: format_field, path_klass: MDL::BorealisAssetMap)
+      @format = format_field
       @path_klass = path_klass
     end
 
     def path
-      path_klass.new(format_field: format).map.new.initial_path
+      path_klass.new(format_field: format_field).map.new.initial_path
     end
   end
 end
