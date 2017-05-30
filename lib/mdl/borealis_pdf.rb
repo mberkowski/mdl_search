1c0825654b1776d2c154593a0df614c9c9e2c4bd
chadfennell <libsys@gmail.com>
2017-05-30 12:02:59 -0700

    new requirement: compound pdfs have their own file url

--- lib/mdl/borealis_pdf.rb
+++ lib/mdl/borealis_pdf.rb
@@ -1,15 +1,27 @@
 module MDL
   class BorealisPDF <  BorealisAsset
      def src
-      "http://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{collection}/id/#{id}/filename"
+      if is_child?
+        "http://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{collection}/id/#{id}/filename"
+      else
+        "http://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{collection}/id/#{parent_id}/filename/#{id}"
+      end
     end
 
     def downloads
-      [src: "http://cdm16022.contentdm.oclc.org/utils/getfile/collection/#{collection}/id/#{id}/filename", label: 'Download']
+      [src: src, label: 'Download']
+    end
+
+    def is_child?
+      parent_id == id
     end
 
     def type
       'pdf'
+    end
+
+    def parent_id
+      document['id'].split(':').last
     end
 
     # PDF viewer requires multiple PDFs, so no to_viewer method here. See
