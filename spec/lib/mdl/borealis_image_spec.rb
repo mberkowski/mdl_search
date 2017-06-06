c1833f7a73340437dc34ca878d766e4a241d79b9
chadfennell <libsys@gmail.com>
2017-06-06 13:11:10 -0700

    updates image tests

--- spec/lib/mdl/borealis_image_spec.rb
+++ spec/lib/mdl/borealis_image_spec.rb
@@ -1,30 +1,31 @@
 require_relative '../../../lib/mdl/borealis_asset.rb'
+require_relative '../../../lib/mdl/borealis_assets_viewer.rb'
 require_relative '../../../lib/mdl/borealis_image.rb'
+require_relative '../../../lib/mdl/borealis_open_seadragon.rb'
 module MDL
   describe BorealisImage do
-    let(:video) { MDL::BorealisImage.new(collection: 'foo', id: 21, transcript: 'Audio transcript here', title: 'Blarg') }
+    let(:image) do
+      MDL::BorealisImage.new collection: 'foo', id: 21
+    end
+
     it 'correctly identifies its src' do
-      expect(video.src).to eq '/contentdm-images/info?id=foo:21'
+      expect(image.src).to eq '/contentdm-images/info?id=foo:21'
     end
 
     it 'correctly identifies its type' do
-      expect(video.type).to eq 'image'
+      expect(image.type).to eq 'image'
     end
 
     it 'correctly identifies its downloads' do
-      expect(video.downloads).to eq [
+      expect(image.downloads).to eq [
         { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/foo/21/full/150,150/0/default.jpg", label: '(150 x 150)' },
         { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/foo/21/full/800,800/0/default.jpg", label: '(800 x 800)' },
         { src: "http://cdm16022.contentdm.oclc.org/digital/iiif/foo/21/full/1920,1920/0/default.jpg", label: '(1920 x 1920)' }
       ]
     end
 
-    it 'serializes itself for use in the viewer' do
-      expect(video.to_viewer).to eq ({})
-    end
-
-    it 'allows a title to be set' do
-      expect(video.title).to eq 'Blarg'
+    it 'knows its player' do
+      expect(image.viewer).to be MDL::BorealisOpenSeadragon
     end
   end
 end
--- spec/lib/mdl/borealis_open_seadragon_spec.rb
+++ spec/lib/mdl/borealis_open_seadragon_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../../lib/mdl/borealis_asset.rb'
+require_relative '../../../lib/mdl/borealis_assets_viewer.rb'
 require_relative '../../../lib/mdl/borealis_image.rb'
 require_relative '../../../lib/mdl/borealis_open_seadragon.rb'
 
@@ -12,7 +13,7 @@ module MDL
       ]
     end
 
-    let(:openseadragon) { BorealisOpenSeadragon.new(images: images) }
+    let(:openseadragon) { BorealisOpenSeadragon.new(assets: images) }
     it 'correctly identifies its src' do
       expect(openseadragon.to_viewer).to eq (
         {
@@ -24,6 +25,7 @@ module MDL
           "label"=>"Image",
           "include_controls"=>true,
           "defaultZoomLevel" => 0,
+          "minZoomLevel" => 0,
           "sequenceMode" => true,
           "showReferenceStrip" => true
         }
