c42cee2bb1f0c87a73224126d9f13153b028298e
chadfennell <libsys@gmail.com>
2017-06-06 12:17:31 -0700

    a pdf spec

--- spec/lib/mdl/borealis_pdf_spec.rb
+++ spec/lib/mdl/borealis_pdf_spec.rb
@@ -1,19 +1,47 @@
+require_relative '../../../lib/mdl/borealis_assets_viewer.rb'
 require_relative '../../../lib/mdl/borealis_asset.rb'
 require_relative '../../../lib/mdl/borealis_pdf.rb'
+require_relative '../../../lib/mdl/borealis_pdf_viewer.rb'
 module MDL
   describe BorealisPDF do
-    let(:video) { MDL::BorealisPDF.new(collection: 'foo', id: 21, transcript: 'Audio transcript here') }
-    it 'correctly identifies its src' do
-      expect(video.src).to eq 'http://cdm16022.contentdm.oclc.org/utils/getfile/collection/foo/id/21/filename'
+    describe 'when a pdf is a member of a compound object' do
+      it 'provides a download link' do
+        expect(BorealisPDF.new(document: { 'id' => 'foo:123' },
+                               collection: 'foo', id: '124').downloads).to eq(
+          [
+            {
+              src: 'http://cdm16022.contentdm.oclc.org/utils/getfile/collection/foo/id/124/filename',
+              label: 'Download'
+            }
+          ]
+        )
+      end
     end
 
-    it 'correctly identifies its downloads' do
-      expect(video.downloads).to eq [{:src=>"http://cdm16022.contentdm.oclc.org/utils/getfile/collection/foo/id/21/filename", :label=>"Download"}]
+    describe 'when a pdf is a single item' do
+      it 'provides a download link' do
+        expect(BorealisPDF.new(document: { 'id' => 'foo:123' },
+                               collection: 'foo', id: '123').downloads).to eq(
+          [
+            {
+              src: 'http://cdm16022.contentdm.oclc.org/utils/getfile/collection/foo/id/123/filename/123',
+              label: 'Download'
+            }
+          ]
+        )
+      end
     end
 
-    it 'serializes itself for use in the viewer' do
-      expect(video.to_viewer).to eq ({"type"=>"pdf", "height"=>800, "thumbnail"=>"/thumbnails/foo:21", "src" => "http://cdm16022.contentdm.oclc.org/utils/getfile/collection/foo/id/21/filename", "transcript"=>{"label"=>"PDF", "texts"=>["Audio transcript here"]}})
+    it 'knows its viewer' do
+      expect(BorealisPDF.new.viewer).to be MDL::BorealisPDFViewer
+    end
+
+    it 'knows its type' do
+      expect(BorealisPDF.new.type).to eq 'pdf'
+    end
+
+    it 'knows its type' do
+      expect(BorealisPDF.new.type).to eq 'pdf'
     end
   end
 end
-
