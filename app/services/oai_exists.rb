70187a2d18231ce8f131c06a3dd2984a452319a4
chadfennell <libsys@gmail.com>
2017-06-08 11:51:44 -0700

    adds service to help identify unpublished records; these will not appear in OAI

---
+++ app/services/oai_exists.rb
@@ -0,0 +1,45 @@
+require 'json'
+class OAIExists
+  attr_reader :collection, :id, :endpoint, :prefix
+  def initialize(collection: '',
+                 id: '',
+                 endpoint: 'http://cdm16022.contentdm.oclc.org/oai/oai.php',
+                 prefix: 'oai:cdm16022.contentdm.oclc.org')
+    @collection = collection
+    @id = id
+    @endpoint = endpoint
+    @prefix = prefix
+  end
+
+  def exists?
+    error?
+  end
+
+  def error?
+    (/The value of the identifier argument is unknown/ =~ error) != nil
+  end
+
+  def error
+    json.fetch('OAI_PMH', {}).fetch('error', '')
+  end
+
+  def json
+    Hash.from_xml(request)
+  end
+
+  def request
+    Net::HTTP.get_response(uri).body
+  end
+
+  def get_record_query
+    "verb=GetRecord&identifier=#{prefix}:#{collection}/#{id}"
+  end
+
+  def uri
+    URI(url)
+  end
+
+  def url
+    "#{endpoint}?#{get_record_query}&metadataPrefix=oai_dc"
+  end
+end
