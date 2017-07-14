de0671dffc78c7e361b2e13cbfc16756e3572a4a
chadfennell <libsys@gmail.com>
2017-07-14 13:31:43 -0500

    cap deploy for stage and prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,45 +1,19 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :domain, "swadm@mtx-reflection-prd.oit.umn.edu"
+role :app, "swadm@mtx-reflection-prd.oit.umn.edu"
+role :web, "swadm@mtx-reflection-prd.oit.umn.edu"
+role :db, "swadm@mtx-reflection-prd.oit.umn.edu", primary: true
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+set :ssh_options, {:forward_agent => true}
 
+set :deploy_to, '/swadm/var/www/mtx-reflection-prd.oit.umn.edu'
+set :use_sudo, false
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
+set :linked_dirs, %w(thumbnails log tmp)
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :rails_env, "production"
 
+set :bundle_flags, '--deployment'
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+set :sidekiq_concurrency, 1
+set :sidekiq_processes, 2
+set :passenger_restart_with_touch, true
