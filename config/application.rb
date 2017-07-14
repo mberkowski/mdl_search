4ed2eb1bdc359b62cf544beee007517501b7467b
chadfennell <libsys@gmail.com>
2017-07-14 10:11:39 -0500

    Rails 5 has autoloading of paths turned of b/c they are not thread safe; tell rails to eager load lib in prod

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module MdlSearch
     config.load_defaults 5.1
 
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
 
